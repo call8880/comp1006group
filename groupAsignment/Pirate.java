ffd7d4235cc75910f58b722d9b357a40d2d90fcc
Clarke <clarkevandervaart@hotmail.com>
2017-12-03 20:04:05 -0500

    Added Player Class

---
+++ groupAsignment/Pirate.java
@@ -0,0 +1,51 @@
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by Clarke on 02/12/2017.
+ */
+public class Pirate extends Player{
+
+
+    public Pirate(World w, String name, Location location, int health, List<Thing> things, Thing goal){
+        super(w, name, location, health, things, goal);
+    }
+
+    public void interact(){
+        System.out.println("The pirate drinks some rum.");
+        this.setHealth(this.getHealth()+15);
+    }
+
+    public void interact(Player p){
+        Random rand = new Random();
+        int  n = rand.nextInt(100) + 1;
+        if(n>50){
+            System.out.println("The drunk pirate tells you to bug off and shoots you.");
+            p.setHealth(p.getHealth()-50);
+        }else{
+            System.out.println("The drunk pirate gives you a gold coin.");
+            p.goal.setValue(p.goal.getValue()+1);
+        }
+    }
+
+    @Override
+    public void play(){
+        int counter = 0;
+        int thisPirate;
+        List<Player> current = w.getRoom(location).getPlayers();
+        for(Player c:current){
+            if(c.equals(this)){
+                current.remove(counter);
+            }
+            counter++;
+        }
+        Random rand = new Random();
+        int  r = rand.nextInt(current.size());
+        if(current.size() >= 1 ){
+            interact(current.get(r));
+        }else{
+            interact();
+        }
+    }
+
+}
