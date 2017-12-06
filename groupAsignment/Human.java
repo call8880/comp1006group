c919544a85d6df09cc46a9c281fcd4e8ea34bca4
callum <callum@192.168.0.13>
2017-12-06 09:10:10 -0500

    .

--- groupAsignment/Human.java
+++ groupAsignment/Human.java
@@ -8,6 +8,7 @@ public class Human extends Player {
 
 	private static boolean verbose = true; // set true for debugging
 	// set false for submitted code
+	private Location loc;
 
 	/**
 	 * Creates a player in the game
@@ -29,6 +30,7 @@ public class Human extends Player {
 	 */
 	public Human(World w, String name, Location location, int health, List<Thing> things, Thing goal) {
 		super(w, name, location, health, things, goal);
+		this.loc = location;
 	}
 
 	/**
